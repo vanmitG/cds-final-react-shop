ef72a5f471f870b873f2d19ae901438d8d023345
vanmitG <mit.van@gmail.com>
2019-09-25 16:00:47 +0700

    add static content to info pages (about, term and condition, contact, faq)

--- src/pages/shops/About.js
+++ src/pages/shops/About.js
@@ -1,11 +1,109 @@
 import React, { Component } from 'react'
-
+import BreadCrumb from "../../components/BreadCrumb"
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <h1>About Page</h1>
-      </div>
+      <>
+        {/* Hello world */}
+        <div className="container">
+          <BreadCrumb page="About Us" />
+          <div className="row">
+            <div className="about-us">
+              <div className="container">
+                <div className="our-story">
+                  <div className="row">
+                    <div className="col-lg-6 col-md-12">
+                      <div className="our-story_text">
+                        <h1 className="title orange-underline">About Us</h1>
+                        <p>Tyna Giang's integrated agro-forestry farming model is the first project in Vietnam to achieve the highest ranking in the "100 projects to combat climate change" by the Ministry of Environment, Energy and Sea. France organized in 2016 ...</p>
+                        <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Neque porro quisquam est, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem</p>
+                        <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Neque porro quisquam est</p>
+                      </div>
+                    </div>
+                    <div className="col-lg-6 col-md-12"> <img src="assets/images/about-img.jpg" className="img-fluid" alt="" title /> </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="clearfix" />
+          </div>
+        </div>
+        <div className="counter-section">
+          <div className="container">
+            <div className="row" id="counter">
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="counter">
+                  <div className="text-center"><img src="assets/images/customer-icon.png" alt="" title className="img-fluid" /></div>
+                  <h2 className="counter-value" data-count={10}>0</h2>
+                  <p className="count-text ">Our Customer</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="counter">
+                  <div className="text-center"><img src="assets/images/customer-icon2.png" alt="" title className="img-fluid" /></div>
+                  <h2 className="counter-value" data-count={25}>0</h2>
+                  <p className="count-text ">Happy Clients</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="counter">
+                  <div className="text-center"><img src="assets/images/customer-icon3.png" alt="" title className="img-fluid" /></div>
+                  <h2 className="counter-value" data-count={150}>0</h2>
+                  <p className="count-text ">Project Complete</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="counter">
+                  <div className="text-center"><img src="assets/images/customer-icon4.png" alt="" title className="img-fluid" /></div>
+                  <h2 className="counter-value" data-count={100}>0</h2>
+                  <p className="count-text">Coffee With Clients</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="why-choose">
+          <h2>Why Choose Us</h2>
+          <div className="clearfix" />
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 mb-4">
+                    <div className="icon-detail"><img src="assets/images/dow_icon_1.png" className="img-fluid" alt="" />
+                      <h5>High Quality Service</h5>
+                      <p>Lorem Ipsum is simply dummy text of the printing</p>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 mb-4">
+                    <div className="icon-detail"><img src="assets/images/dow_icon_2.png" alt="" />
+                      <h5>Flexibility</h5>
+                      <p>Lorem Ipsum is simply dummy text of the printing</p>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 mb-4">
+                    <div className="icon-detail"><img src="assets/images/dow_icon_3.png" alt="" />
+                      <h5>Reliable Consistent Supply</h5>
+                      <p>Lorem Ipsum is simply dummy text of the printing</p>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 mb-4">
+                    <div className="icon-detail"><img src="assets/images/dow_icon_4.png" alt="" />
+                      <h5>Customer Planning and Management</h5>
+                      <p>Lorem Ipsum is simply dummy text of the printing</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-5">
+                <div><img src="assets/images/page-img/why-choose.jpg" title alt="" className="img-fluid" /></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="clearfix" />
+        {/* End */}
+      </>
     )
   }
 }
--- src/pages/shops/FAQ.js
+++ src/pages/shops/FAQ.js
@@ -1,11 +1,72 @@
 import React, { Component } from 'react'
-
+import BreadCrumb from "../../components/BreadCrumb"
 export default class FAQ extends Component {
   render() {
     return (
-      <div>
-        <h1>FAQ Pages</h1>
-      </div>
+      <>
+        {/* Hello world */}
+        <div className="container">
+          <BreadCrumb page="FAQ" />
+          <div className="row">
+            <div className="col-lg-12 mt-2 mb-5">
+              <div id="accordion" role="tablist" className="faq">
+                <div className="card">
+                  <div className="card-header" role="tab" id="headingOne"> <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <h5 className="mb-0"> Lorem Ipsum is simply dummy text of the printing. ? </h5>
+                  </a> </div>
+                  <div id="collapseOne" className="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div className="card-body">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-header" role="tab" id="headingTwo"> <a className="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <h5 className="mb-0"> Lorem Ipsum has been the industry's standard dummy ? </h5>
+                  </a> </div>
+                  <div id="collapseTwo" className="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+                    <div className="card-body">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-header" role="tab" id="headingThree"> <a className="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <h5 className="mb-0"> Many desktop publishing packages and web page editors now use Lorem  ? </h5>
+                  </a> </div>
+                  <div id="collapseThree" className="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+                    <div className="card-body">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-header" role="tab" id="heading4"> <a className="collapsed" data-toggle="collapse" href="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                    <h5 className="mb-0"> Many desktop publishing packages and web page editors now use Lorem  ? </h5>
+                  </a> </div>
+                  <div id="collapse4" className="collapse" role="tabpanel" aria-labelledby="heading4" data-parent="#accordion">
+                    <div className="card-body">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-header" role="tab" id="heading5"> <a className="collapsed" data-toggle="collapse" href="#collapse5" aria-expanded="false" aria-controls="collapse5">
+                    <h5 className="mb-0"> Many desktop publishing packages and web page editors now use Lorem  ? </h5>
+                  </a> </div>
+                  <div id="collapse5" className="collapse" role="tabpanel" aria-labelledby="heading5" data-parent="#accordion">
+                    <div className="card-body">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="clearfix" />
+        {/* End */}
+      </>
     )
   }
 }
