82a9165ef96dca38a51e8fd0c070e2b2c1c54590
vanmitG <mit.van@gmail.com>
2019-09-29 18:27:42 +0700

    add cart reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import productReducer from './productReducer'
+<<<<<<< HEAD
 import cartReducer from './cartReducer';
 import orderReducer from './orderReducer';
 import wishListReducer from './wishListReducer';
@@ -12,3 +13,9 @@ export default combineReducers({
 });
 
 
+=======
+
+export default combineReducers({
+  products: productReducer
+});
+>>>>>>> ab6932cae0e01f3a6a48d092de249392339a2769
